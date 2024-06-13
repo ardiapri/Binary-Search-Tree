df34b9748bfaca9ce5f8c8b00f6d381559bf61f4
ardiapri <ardiapri882@gmail.com>
2024-06-13 19:20:04 +0700

    switch

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -123,4 +123,19 @@ int main() {
 		cout << "5. Exit" << endl;
 		cout << "\nEnter your choice (1-5) : ";
 
+		char ch;
+		cin >> ch;
+		cout << endl;
 		
+		switch (ch)
+		{
+			case '1': 
+			{
+				cout << "Enter a word: ";
+				string word;
+				cin >> word;
+				x.insert(word);
+				break;
+			}
+			
+};
