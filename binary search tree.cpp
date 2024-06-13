df9c82bee31e7abc8e0e4d4dada550166edbfc92
ardiapri <ardiapri882@gmail.com>
2024-06-13 18:52:34 +0700

    void inorder

--- binary search tree.cpp
+++ binary search tree.cpp
@@ -68,5 +68,19 @@ public:
 				currentNode = currentNode->rightchild; 
 		}
 	}
-	
+	void inorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empety" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
+
 };
